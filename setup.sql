65d26252aee3fe6ff05eab16388368bf331d3e98
mousastech <mousastech>
2024-11-20 19:55:24 +0000

    Pipeline Initial

---
+++ setup.sql
@@ -0,0 +1,16 @@
+-- Databricks notebook source
+CREATE CATALOG IF NOT EXISTS ecommerce;
+
+CREATE EXTERNAL VOLUME IF NOT EXISTS files 
+LOCATION 's3://dlt-ecommerce/' 
+COMMENT 'External volume for raw files';
+
+CREATE SCHEMA IF NOT EXISTS `ecommerce`.`bbronze`;
+
+CREATE SCHEMA IF NOT EXISTS `ecommerce`.`silver`;
+
+CREATE SCHEMA IF NOT EXISTS `ecommerce`.`gold`;
+
+-- COMMAND ----------
+
+USE CATALOG ecommerce;
